260cd87917db928f04f3c790e2db978e8c35ca12
lRethi <azraethly@gmail.com>
2025-03-17 10:35:38 -0300

    TesteClone

--- cloneGit.cpp
+++ cloneGit.cpp
@@ -7,12 +7,13 @@ int main() {
     setlocale(LC_ALL, "pt_BR.utf8"); //Para usar acentuação
     cout << "\033[1;34m***************************************************\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
-    cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS!                     \033[1;32m          *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;33m PARABÉNS, BRAYAN 34DS!                     \033[1;32m          *\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*   \033[1;36mpara o seu repositório local!                 \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;34m***************************************************\033[0m\n";
+    cout << "Valeu, professor!" << endl;
 
     return 0;
 }
